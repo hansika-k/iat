5e52c054e1f464653825a5e49fee079aa77b3191
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-02-23 11:35:55 +0530

    Update touchtrue.js

--- touchtrue.js
+++ touchtrue.js
@@ -1314,11 +1314,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 
 		return API.script;
 	}
-
-	return iatExtension;
-});
-
-showDebriefing:false, //Show feedback in the last trial? Relevant only in a Qualtrics IAT because in Qualtrics we cannot access the saved feedback and IAT score later in the survey.
+	showDebriefing:true, //Show feedback in the last trial? Relevant only in a Qualtrics IAT because in Qualtrics we cannot access the saved feedback and IAT score later in the survey.
 //Texts for the trials that show the debriefing.
 preDebriefingText : 'Press space to see your result', //Text in the trial that comes before showing the debriefing.
 preDebriefingTouchText : 'Touch the bottom green area to see your result', //Touch version for the text in the trial that comes before showing the debriefing.
@@ -1327,3 +1323,7 @@ debriefingTextTop : 'Your result:', //Will be shown above the feedback text.
 //In Project Implicit's website, you can see that we added much text to explain that there is still much unknown about the meaning of these results.
 //We strongly recommend that you provide all these details in the debriefing of the experiment.
 debriefingTextBottom : 'This result is not a definitive assessment of your attitudes. It is provided for educational purposes only.', //Will be shown below the feedback text. 
+
+	return iatExtension;
+});
+
