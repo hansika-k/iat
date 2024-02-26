9711b7d4c2e8e13c0a7c32dd6c83e4450dd1b520
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-02-26 15:25:51 +0530

    Update ibmcd.js

--- ibmcd.js
+++ ibmcd.js
@@ -11,11 +11,11 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue.js']
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-            {image: 'IB1.png'},
-				{image: 'IB2.png'},
-				{image: 'IB3.png'},
-				{image: 'IB4.png'},
-				{image: 'IB5.png'}
+            {image: 'IB1.jpg'},
+				{image: 'IB2.jpg'},
+				{image: 'IB3.jpg'},
+				{image: 'IB4.jpg'},
+				{image: 'IB5.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
@@ -32,7 +32,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue.js']
 				{image: 'McD2.jpg'},
 				{image: 'McD3.jpg'},
 				{image: 'McD4.jpg'},
-				{image: 'McD5.png'}
+				{image: 'McD5.jpg'}
 			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
@@ -75,7 +75,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue.js']
 		},
 
 		base_url : {//Where are your images at?
-			image : 'https://github.com/hansika-k/iat/tree/main/images/'
+			image : 'https://github.com/hansika-k/iat/tree/main/imagesjpg/'
 		} 
 	});
 });
