e4dbb5fa5a8e7774383f726982024e06d2560542
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-04-01 14:53:40 +0530

    Update itc1.js

--- itc1.js
+++ itc1.js
@@ -4,71 +4,71 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue.js']
 	return iatExtension({
 
 		category1 : {
-			name : 'Positive', //Will appear in the data.
+			name : 'Packaged', //Will appear in the data.
 			title : {
-				media : {word : 'Positive'}, //Name of the category presented in the task.
+				media : {word : 'Packaged'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-            {word: 'hygienic'},
-				{word: 'nutrituous'},
-				{word: 'healthy'},
-				{word: 'fresh'},
-				{word: 'tasty'}
+            {word: 'packaged1.jpg'},
+				{word: 'packaged2.jpg'},
+				{word: 'packaged3.jpg'},
+				{word: 'packaged4.jpg'},
+				{word: 'packaged5.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'Negative', //Will appear in the data.
+			name : 'Chakki', //Will appear in the data.
 			title : {
-				media : {word : 'Negative'}, //Name of the category presented in the task.
+				media : {word : 'Chakki'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-           	{word: 'unhygienic'},
-				{word: 'impure'},
-				{word: 'unhealthy'},
-				{word: 'stale'},
-				{word: 'rotten'}
+           	{word: 'chakki1.jpg'},
+				{word: 'chakki1.jpg'},
+				{word: 'chakki1.jpg'},
+				{word: 'chakki1.jpg'},
+				{word: 'chakki1.jpg'}
 			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},
 		attribute1 :
 		{
-			name : 'Packaged',
+			name : 'Positive',
 			title : {
-				media : {word : 'Packaged'},
+				media : {word : 'Positive'},
 				css : {color:'#0000FF','font-size':'1.8em'},
 				height : 4 //Used to position the "Or" in the combined block.
 			},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
- {image: 'packaged1.jpg'},
-				{image: 'packaged2.jpg'},
-				{image: 'packaged3.jpg'},
-				{image: 'packaged4.jpg'},
-				{image: 'packaged5.jpg'}
+ {image: 'healthy'},
+				{image: 'hygienic'},
+				{image: 'nutritious'},
+				{image: 'fresh'},
+				{image: 'tasty'}
 			], 
 			//Stimulus css
 			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 		},
 		attribute2 :
 		{
-			name : 'Chakki',
+			name : 'Negative',
 			title : {
-				media : {word : 'Chakki'},
+				media : {word : 'Negative'},
 				css : {color:'#0000FF','font-size':'1.8em'},
 				height : 4 //Used to position the "Or" in the combined block.
 			},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-  {image: 'chakki1.jpg'},
-				{image: 'chakki2.jpg'},
-				{image: 'chakki3.jpg'},
-				{image: 'chakki4.jpg'},
-				{image: 'chakki5.jpg'}
+  {image: 'unhealthy'},
+				{image: 'stale'},
+				{image: 'rotten'},
+				{image: 'impure'},
+				{image: 'unhygienic'}
 			], 
 			//Stimulus css
 			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
