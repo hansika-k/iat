8a1598b4398a1b5f912cf1bc2ddab834e11d35c8
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-04-03 12:52:17 +0530

    Create unilever2.js

---
+++ unilever2.js
@@ -0,0 +1,81 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+
+		category1 : {
+			name : 'Hygiene', //Will appear in the data.
+			title : {
+				media : {word : 'Hygiene'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+            {word: 'clean'},
+				{word: 'pure'},
+				{word: 'healthy'},
+				{word: 'sanitary'},
+				{word: 'germ-free'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Beauty', //Will appear in the data.
+			title : {
+				media : {word : 'Beauty'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+           	{word: 'parlor'},
+				{word: 'spa'},
+				{word: 'cosmetics'},
+				{word: 'haircare'},
+				{word: 'skincare'}
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},
+		attribute1 :
+		{
+			name : 'Necessity',
+			title : {
+				media : {word : 'Necessity'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+            	{word: 'food'},
+				{word: 'water'},
+				{word: 'essential'},
+				{word: 'basic'},
+				{word: 'need'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'Indulgence',
+			title : {
+				media : {word : 'Indulgence'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+          {word: 'luxury'},
+				{word: 'treat'},
+				{word: 'extravagence'},
+				{word: 'excess'},
+				{word: 'pampering'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://hansika-k.github.io/iat/imagesjpg/'
+		} 
+	});
+});
