e7f4aa9a3043a6a55dee72c0c8d6909c3e8423f4
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-04-03 13:01:43 +0530

    Create unilever3.js

---
+++ unilever3.js
@@ -0,0 +1,81 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+
+		category1 : {
+			name : 'Soap', //Will appear in the data.
+			title : {
+				media : {word : 'Soap'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+            {image: 'soap1.jpg'},
+				{image: 'soap2.jpg'},
+				{image: 'soap3.jpg'},
+				{image: 'soap4.jpg'},
+				{image: 'soap5.png'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Body Wash', //Will appear in the data.
+			title : {
+				media : {word : 'Body Wash'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+           	{image: 'body1.jpg'},
+				{image: 'body2.png'},
+				{image: 'body3.jpg'},
+				{image: 'body4.jpg'},
+				{image: 'body5.jpg'}
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},
+		attribute1 :
+		{
+			name : 'Hygiene',
+			title : {
+				media : {word : 'Hygiene'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+            	{word: 'clean'},
+				{word: 'pure'},
+				{word: 'healthy'},
+				{word: 'sanitary'},
+				{word: 'germ-free'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'Beauty',
+			title : {
+				media : {word : 'Beauty'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+          {word: 'parlor'},
+				{word: 'spa'},
+				{word: 'cosmetics'},
+				{word: 'haircare'},
+				{word: 'skincare'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://hansika-k.github.io/iat/imagesjpg/'
+		} 
+	});
+});
