03c3787660e4ba9988fc86e591e519067a0a7ad1
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-04-16 10:34:19 +0530

    Update and rename unilever1.js to unilever1.1.js

--- unilever1.1.js
+++ unilever1.1.js
@@ -4,58 +4,40 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue.js']
 	return iatExtension({
 
 		category1 : {
-			name : 'Male', //Will appear in the data.
+			name : 'Self', //Will appear in the data.
 			title : {
-				media : {word : 'Male'}, //Name of the category presented in the task.
+				media : {word : 'Self'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-            {word: 'Rahul'},
-				{word: 'Rohit'},
-				{word: 'Abhishek'},
-				{word: 'Anand'},
-				{word: 'Manish'}
+            {word: 'myself'},
+				{word: 'me'},
+				{word: 'mine'},
+				{word: 'I'},
+				{word: 'oneself'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'Female', //Will appear in the data.
+			name : 'Others', //Will appear in the data.
 			title : {
-				media : {word : 'Female'}, //Name of the category presented in the task.
+				media : {word : 'Others'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-           	{word: 'Ayushi'},
-				{word: 'Tanya'},
-				{word: 'Riya'},
-				{word: 'Deepika'},
-				{word: 'Neha'}
+           	{word: 'family'},
+				{word: 'parents'},
+				{word: 'in-laws'},
+				{word: 'children'},
+				{word: 'husband'}
 			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},
 		attribute1 :
-		{
-			name : 'Unhygienic',
-			title : {
-				media : {word : 'Unhygienic'},
-				css : {color:'#0000FF','font-size':'1.8em'},
-				height : 4 //Used to position the "Or" in the combined block.
-			},
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-            	{word: 'dirty'},
-				{word: 'impure'},
-				{word: 'sick'},
-				{word: 'filthy'},
-				{word: 'stained'}
-			],
-			//Stimulus css
-			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-		},
-		attribute2 :
 		{
 			name : 'Hygienic',
 			title : {
@@ -64,11 +46,29 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue.js']
 				height : 4 //Used to position the "Or" in the combined block.
 			},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-          {word: 'clean'},
+            	{word: 'clean'},
 				{word: 'pure'},
 				{word: 'healthy'},
-				{word: 'sanitary'},
-				{word: 'germ-free'}
+				{word: 'germ-free'},
+				{word: 'sterilized'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'Unygienic',
+			title : {
+				media : {word : 'Unygienic'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+          {word: 'dirty'},
+				{word: 'impure'},
+				{word: 'sick'},
+				{word: 'filthy'},
+				{word: 'stained'}
 			],
 			//Stimulus css
 			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
