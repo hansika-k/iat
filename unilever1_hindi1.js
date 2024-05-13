aabe03a1fa8b2e6b497f7f5a2715e212c41d181b
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-05-13 17:37:56 +0530

    Update and rename unilever1_hindi.js to unilever1_hindi1.js

--- unilever1_hindi1.js
+++ unilever1_hindi1.js
@@ -4,58 +4,40 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue_hind
 	return iatExtension({
 
 		category1 : {
-			name : 'पुरुष', //Will appear in the data.
+			name : 'मैं', //Will appear in the data.
 			title : {
-				media : {word : 'पुरुष'}, //Name of the category presented in the task.
+				media : {word : 'मैं'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-            {word: 'राहुल'},
-				{word: 'रोहित'},
-				{word: 'अभिषेक'},
-				{word: 'आनंद'},
-				{word: 'मनीष'}
+            {word: 'अपना'},
+				{word: 'स्वयं'},
+				{word: 'मेरा'},
+				{word: 'खुद'},
+				{word: 'स्वत:'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'महिला', //Will appear in the data.
+			name : ' दूसरे', //Will appear in the data.
 			title : {
-				media : {word : 'महिला'}, //Name of the category presented in the task.
+				media : {word : ' दूसरे'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-           	{word: 'आयुषी'},
-				{word: 'तान्या'},
-				{word: 'रिया'},
-				{word: 'दीपिका'},
-				{word: 'नेहा'}
+           	{word: 'परिवार'},
+				{word: 'सास-ससुर'},
+				{word: 'पति'},
+				{word: 'बच्चे'},
+				{word: 'वो'}
 			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},
 		attribute1 :
-		{
-			name : 'अस्वच्छ',
-			title : {
-				media : {word : 'अस्वच्छ'},
-				css : {color:'#0000FF','font-size':'1.8em'},
-				height : 4 //Used to position the "Or" in the combined block.
-			},
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-            	{word: 'गंदा'},
-				{word: 'अशुद्ध'},
-				{word: 'बीमार'},
-				{word: 'मैला'},
-				{word: 'दाग'}
-			],
-			//Stimulus css
-			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
-		},
-		attribute2 :
 		{
 			name : 'स्वच्छ',
 			title : {
@@ -64,11 +46,29 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue_hind
 				height : 4 //Used to position the "Or" in the combined block.
 			},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-          {word: 'साफ'},
+            	{word: 'साफ'},
 				{word: 'शुद्ध'},
 				{word: 'स्वस्थ'},
-				{word: 'सेनेटरी'},
-				{word: 'रोगाणुमुक्त'}
+				{word: 'संक्रामण रहित'},
+				{word: 'कीटानुरहित'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'अस्वच्छ',
+			title : {
+				media : {word : 'अस्वच्छ'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+          {word: 'गंदा'},
+				{word: 'अशुद्ध'},
+				{word: 'बीमार'},
+				{word: 'मैला'},
+				{word: 'दाग'}
 			],
 			//Stimulus css
 			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
