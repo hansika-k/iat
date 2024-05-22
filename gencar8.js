6c52c092ec61dfc9b9452933f53ad5316e7522d3
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-05-22 18:28:01 +0530

    Update and rename gencar7.js to gencar8.js

--- gencar8.js
+++ gencar8.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue_feedback3.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue_feedback4.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
