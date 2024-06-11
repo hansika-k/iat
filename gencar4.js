2798dd23deb9d3fce0d534ae79d5f3f22849f632
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-06-11 15:46:47 +0530

    Update and rename gencar3.js to gencar4.js

--- gencar4.js
+++ gencar4.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/minnoiat.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue_nooverlap_marathi1.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
