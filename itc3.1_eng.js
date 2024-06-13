0d0f7ff02870cbc7a417277632dafbb5af5ebd03
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-06-13 15:46:24 +0530

    Create itc3.1_eng.js

---
+++ itc3.1_eng.js
@@ -0,0 +1,81 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue_nooverlap.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+
+		category1 : {
+			name : 'Packaged', //Will appear in the data.
+			title : {
+				media : {word : 'Packaged'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+            {image: 'packaged1.jpg'},
+				{image: 'packaged2.jpg'},
+				{image: 'packaged3.jpg'},
+				{image: 'packaged4.jpg'},
+				{image: 'packaged5.jpg'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Chakki', //Will appear in the data.
+			title : {
+				media : {word : 'Chakki'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+           	{image: 'chakki1.jpg'},
+				{image: 'chakki2.jpg'},
+				{image: 'chakki3.jpg'},
+				{image: 'chakki4.jpg'},
+				{image: 'chakki5.jpg'}
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},
+		attribute1 :
+		{
+			name : 'Western',
+			title : {
+				media : {word : 'Western'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+            	{word: 'frankie'},
+				{word: 'pizza'},
+				{word: 'bread'},
+				{word: 'cake'},
+				{word: 'tacos'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'Indian',
+			title : {
+				media : {word : 'Indian'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+          {word: 'roti'},
+				{word: 'puri'},
+				{word: 'paratha'},
+				{word: 'samosa'},
+				{word: 'thepla'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://hansika-k.github.io/iat/imagesjpg/'
+		} 
+	});
+});
