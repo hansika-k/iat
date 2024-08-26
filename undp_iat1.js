7fd3534f985f2833b4bc5aeed5e5ba443029940e
hansika-k <63161832+hansika-k@users.noreply.github.com>
2024-08-26 09:16:16 +0530

    Create undp_iat1.js

---
+++ undp_iat1.js
@@ -0,0 +1,73 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/hansika-k/iat@main/touchtrue_nooverlap.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+
+		category1 : {
+			name : 'Man', //Will appear in the data.
+			title : {
+				media : {word : 'Man'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'Tom'},
+				{word: 'Ling'},
+				{word: 'Somsak'},
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Woman', //Will appear in the data.
+			title : {
+				media : {word : 'Woman'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'Jane'},
+				{word: 'Jing'},
+				{word: 'Cho'},
+			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},
+		attribute1 :
+		{
+			name : 'Analytical',
+			title : {
+				media : {word : 'Analytical'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+            	{word: 'precise'},
+				{word: 'systematic'},
+				{word: 'logical'},
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+		attribute2 :
+		{
+			name : 'Intuitive',
+			title : {
+				media : {word : 'Intuitive'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+          {word: 'instictive'},
+				{word: 'spontaneous'},
+				{word: 'automatic'},
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://hansika-k.github.io/iat/imagesjpg/'
+		},
+	});
+});
